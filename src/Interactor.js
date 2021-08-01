94a66022a2f12731d070ce1d18fddd93baddb51b
Danijel Radulović <master.daca09@gmail.com>
2021-08-02 00:01:04 +0200

    Add RPC getDebt and getAllowedBorrowingAmount

--- src/Interactor.js
+++ src/Interactor.js
@@ -113,7 +113,37 @@ function Main (props) {
               }
             ],
             section: 'defiModule',
-            type: 'Balance'
+            type: 'BalanceInfo'
+          },
+          getDebt: {
+            alias: ['get_debt'],
+            description: 'RPC for getting user debt',
+            jsonrpc: 'defiModule_getDebt',
+            method: 'getDebt',
+            isSubscription: false,
+            params: [
+              {
+                name: 'user',
+                type: 'AccountId'
+              }
+            ],
+            section: 'defiModule',
+            type: 'BalanceInfo'
+          },
+          getAllowedBorrowingAmount: {
+            alias: ['get_allowed_borrowing_amount'],
+            description: 'RPC for getting user allowed borrowing amount',
+            jsonrpc: 'defiModule_getAllowedBorrowingAmount',
+            method: 'getAllowedBorrowingAmount',
+            isSubscription: false,
+            params: [
+              {
+                name: 'user',
+                type: 'AccountId'
+              }
+            ],
+            section: 'defiModule',
+            type: 'BorrowingInfo'
           }
         }
       };
--- src/substrate-lib/SubstrateContext.js
+++ src/substrate-lib/SubstrateContext.js
@@ -79,6 +79,10 @@ const connect = (state, dispatch) => {
     },
     BalanceInfo: {
       balance: 'Balance'
+    },
+    BorrowingInfo: {
+      borrowing_balance: 'Balance',
+      allowed_borrowing_amount: 'Balance'
     }
   };
 
@@ -99,6 +103,36 @@ const connect = (state, dispatch) => {
         ],
         section: 'defiModule',
         type: 'BalanceInfo'
+      },
+      getDebt: {
+        alias: ['get_debt'],
+        description: 'RPC for getting user debt',
+        jsonrpc: 'defiModule_getDebt',
+        method: 'getDebt',
+        isSubscription: false,
+        params: [
+          {
+            name: 'user',
+            type: 'AccountId'
+          }
+        ],
+        section: 'defiModule',
+        type: 'BalanceInfo'
+      },
+      getAllowedBorrowingAmount: {
+        alias: ['get_allowed_borrowing_amount'],
+        description: 'RPC for getting user allowed borrowing amount',
+        jsonrpc: 'defiModule_getAllowedBorrowingAmount',
+        method: 'getAllowedBorrowingAmount',
+        isSubscription: false,
+        params: [
+          {
+            name: 'user',
+            type: 'AccountId'
+          }
+        ],
+        section: 'defiModule',
+        type: 'BorrowingInfo'
       }
     }
   };
